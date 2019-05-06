d92ee774a95d644a349fb5097bb9bfa5ce622acd
AGOULZI mohamed <mohammed.agoulzi50@gmail.com>
2019-05-06 18:20:26 +0000

    done

--- front/src/app/special-events/special-events.component.ts
+++ front/src/app/special-events/special-events.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { EventsService } from '../events.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-special-events',
@@ -8,13 +10,20 @@ import { EventsService } from '../events.service';
 })
 export class SpecialEventsComponent implements OnInit {
   specials = [];
-  constructor(private eventsService: EventsService) { }
+  constructor(private eventsService: EventsService,
+  private router:Router) { }
   
 
   ngOnInit() {
     this.eventsService.getSpecial().subscribe(
       res => this.specials = res,
-      err=>console.log(err)
+      err => {
+        if (err instanceof HttpErrorResponse) {
+          if (err.status === 401)
+            this.router.navigate(['/login'])
+        }
+          
+      }
     )
   }
 
