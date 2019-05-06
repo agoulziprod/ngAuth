796ffb969653710c6991fa6c4406283fd8525870
AGOULZI mohamed <mohammed.agoulzi50@gmail.com>
2019-05-06 15:42:31 +0000

    login + register done

--- front/src/app/login/login.component.ts
+++ front/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  loginUserData = {};
 
-  constructor() { }
+  constructor(private auth:AuthService) { }
 
   ngOnInit() {
+  }
+
+  loginUser() {
+    this.auth.loginUser(this.loginUserData)
+      .subscribe(res => console.table(res), err=>console.table(err))
   }
 
 }
